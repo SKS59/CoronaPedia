e99414f39e053aa7d0800e4b6c82d3f82029c4c8
SKS59 <55923974+SKS59@users.noreply.github.com>
2020-05-07 12:20:07 +0530

    Update adsproject.cpp

--- adsproject.cpp
+++ adsproject.cpp
@@ -28,6 +28,14 @@ int main()
             while(infile.good())
             {
                 getline(infile,stateName,',');
+                if(stateName=="state")
+                {
+                    getline(infile,confirmed,',');
+                    getline(infile,recovered,',');
+                    getline(infile,deaths,',');
+                    getline(infile,active,'\n');
+                    continue;
+                }
                 getline(infile,confirmed,',');
                 getline(infile,recovered,',');
                 getline(infile,deaths,',');
@@ -45,23 +53,28 @@ int main()
             flag=false;
             while(getline(infile,stateName,',') && !flag)
             {
+                if(stateName=="state")
+                {
+                    getline(infile,confirmed,',');
+                    getline(infile,recovered,',');
+                    getline(infile,deaths,',');
+                    getline(infile,active,'\n');
+                    continue;
+                }
                 getline(infile,confirmed,',');
                 getline(infile,recovered,',');
                 getline(infile,deaths,',');
                 getline(infile,active,'\n');
+                cout<<stateName;
                 if(temp==stateName)
                 {
                     cout<<"\nSTATE : "<<stateName<<"\nCONFIRMED : "<<confirmed<<"\nRECOVERED : "<<recovered<<"\nDEATHS : "<<deaths<<"\nACTIVE : "<<active<<endl;
+                    flag=true;
                 }
             }
             infile.close();
             break;
         }
-            stateName="";
-            recovered="";
-            deaths="";
-            active="";
-            confirmed="";
         cout<<"\nDo you want to continue ? ";
         cin>>yn;
     }while(yn=='y'||yn!='Y');
